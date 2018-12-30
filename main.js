48eca9e7e6dbd3e8d0116108be7743af9fd6bff5
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2018-12-30 02:17:22 +0100

    formatting js

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var input = document.getElementById("inlink");
-    input.addEventListener("keyup", function(event){
-    event.preventDefault();
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) 
-    {
-        // Trigger the button element with a click
-        document.getElementById("btn113fy").click();
-    }
+    input.addEventListener("keyup", function (event) {
+        event.preventDefault();
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            // Trigger the button element with a click
+            document.getElementById("btn113fy").click();
+        }
     });
 }
 );
 
 
 //this is the main function, which gets the link and 113fies it.
-function _113fy()
-{
+function _113fy() {
     var link = document.getElementById("inlink").value;
     var cleanlink = cleanLink(link);
-	var splittedbyscores = cleanlink.split("-");
-	var lastscore = splittedbyscores[splittedbyscores.length - 1];
-	var splittedbydots = lastscore.split(".");
+    var splittedbyscores = cleanlink.split("-");
+    var lastscore = splittedbyscores[splittedbyscores.length - 1];
+    var splittedbydots = lastscore.split(".");
     var code = splittedbydots[0];
-    
-    var newlink = cleanlink + "?utm_source=facebook_fb01a&utm_medium=facebook_fb01a_113&utm_campaign=fb01a_"+code+"_113";
-    
+
+    var newlink = cleanlink + "?utm_source=facebook_fb01a&utm_medium=facebook_fb01a_113&utm_campaign=fb01a_" + code + "_113";
+
     document.getElementById("outlink").value = newlink;
 
     //code ends here
@@ -32,25 +30,21 @@ function _113fy()
     displayNone();
 }
 
-function cleanAll()
-{
+function cleanAll() {
     displayNone();
     document.getElementById("inlink").value = "";
     document.getElementById("outlink").value = "";
 }
 
 //a function that builds the fidelity search query
-function fidelityURLBuilder()
-{
+function fidelityURLBuilder() {
     var link = document.getElementById("infidelitylink").value;
     var splittedlink = link.split(" ");
     var dividedbyscoreslink = "https://news.fidelityhouse.eu/?s=";
 
-    for(var i = 0; i<splittedlink.length; i++)
-    {
+    for (var i = 0; i < splittedlink.length; i++) {
         dividedbyscoreslink += splittedlink[i];
-        if(i != splittedlink.length - 1)
-        {
+        if (i != splittedlink.length - 1) {
             dividedbyscoreslink += "+";
         }
     }
@@ -68,65 +62,61 @@ function popitup(url) {
 
     var left = ((width / 2) - (600 / 2));
     var top = ((height / 2) - (600 / 2));
-	newwindow=window.open(url,'name','height=650,width=650,top='+ top + ',left='+left);
-	if (window.focus) {newwindow.focus()}
-	return false;
+    newwindow = window.open(url, 'name', 'height=650,width=650,top=' + top + ',left=' + left);
+    if (window.focus) { newwindow.focus() }
+    return false;
 }
 
 //to clean dirty links
-function cleanLink(link)
-{
+function cleanLink(link) {
     var splittedbyquestionmark = link.split("?");
     return splittedbyquestionmark[0];
 }
 
 //function used to copy the generated link content
 function ToClipboard() {
-    
-    if(iOS())
-    {
+
+    if (iOS()) {
         iosCopyToClipboard();
     }
-    else
-    {
+    else {
         WindowsCopyToClipboard();
     }
 
     displayBlock();
-  }
+}
 
 //copy function that works on windows
-function WindowsCopyToClipboard()
-  {
-      /* Get the text field */
-      var copyText = document.getElementById("outlink");
-    
-      /* Select the text field */
-      copyText.select();
-  
-      /* Copy the text inside the text field */
-      document.execCommand("copy");
-  }
+function WindowsCopyToClipboard() {
+    /* Get the text field */
+    var copyText = document.getElementById("outlink");
+
+    /* Select the text field */
+    copyText.select();
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+}
 
 //check if the device is using iOS
 function iOS() {
     var iDevices = [
-      'iPad Simulator',
-      'iPhone Simulator',
-      'iPod Simulator',
-      'iPad',
-      'iPhone',
-      'iPod'
+        'iPad Simulator',
+        'iPhone Simulator',
+        'iPod Simulator',
+        'iPad',
+        'iPhone',
+        'iPod'
     ];
-  
+
     if (!!navigator.platform) {
-      while (iDevices.length) {
-        if (navigator.platform === iDevices.pop()){ return true; }
-      }
+        while (iDevices.length) {
+            if (navigator.platform === iDevices.pop()) { return true; }
+        }
     }
-  
+
     return false;
-  }
+}
 
 //copy function that works on iOS
 function iosCopyToClipboard() {
